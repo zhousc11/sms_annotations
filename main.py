a0b748cd95e44653e630d3bb152ef0d98507ca9a
zhousc66 <zhousc11@icloud.com>
2024-06-12 21:38:48 +0800

    ok2

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import json
 # 星火认知大模型Spark3.5 Max的URL值，其他版本大模型URL值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
 SPARKAI_URL = 'wss://spark-api.xf-yun.com/v3.5/chat'
 # 星火认知大模型调用秘钥信息，请前往讯飞开放平台控制台（https://console.xfyun.cn/services/bm35）查看
-SPARKAI_APP_ID = 'SECRET'
-SPARKAI_API_SECRET = 'SECRET'
-SPARKAI_API_KEY = 'SECRET'
+SPARKAI_APP_ID = ''
+SPARKAI_API_SECRET = ''
+SPARKAI_API_KEY = ''
 # 星火认知大模型Spark3.5 Max的domain值，其他版本大模型domain值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
 SPARKAI_DOMAIN = 'generalv3.5'
 
